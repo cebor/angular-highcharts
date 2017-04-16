652bcc82e2ee46ef66c0d76844c51df9300fbeea
Felix Itzenplitz <felix@stkn.org>
2017-04-16 18:50:59 +0200

    add bump script

---
+++ bump.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
+cd src
+
+VERSION=$(npm version "$1" -m "bump version to %s")
+MESSAGE="bump version to `echo $VERSION | cut -c2-`"
+git commit -m "$MESSAGE" && git tag $VERSION -m "$MESSAGE"
