f64e7c668ca53937cd982c70585d1ecdbc648d67
Felix Itzenplitz <felix@stkn.org>
2017-04-16 18:53:40 +0200

    bump version to 3.0.1

--- bump.sh
+++ bump.sh
@@ -5,4 +5,4 @@ cd src
 
 VERSION=$(npm version "$1" -m "bump version to %s")
 MESSAGE="bump version to `echo $VERSION | cut -c2-`"
-git commit -m "$MESSAGE" && git tag $VERSION -m "$MESSAGE"
+git commit -a -m "$MESSAGE" && git tag $VERSION -m "$MESSAGE"
