cf45e6ba70b6abab367c899c2e4f04cc8d897cf0
Felix Itzenplitz <felix@stkn.org>
2017-10-19 20:15:18 +0200

    add license

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,3 +1,12 @@
+/**
+ * @license
+ * Copyright Felix Itzenplitz. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at
+ * https://github.com/cebor/angular-highcharts/blob/master/LICENSE
+ */
+
 export { ChartModule } from './chart.module';
 export { ChartDirective } from './chart.directive';
 export { HIGHCHARTS_MODULES } from './chart.service';
