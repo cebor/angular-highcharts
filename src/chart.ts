136e6997d0789341d0e09452e032f61ce56dcaf8
Felix Itzenplitz <felix@stkn.org>
2018-01-24 01:14:19 +0100

    small fixes

--- src/chart.ts
+++ src/chart.ts
@@ -30,7 +30,7 @@ export class Chart {
    * Add Point
    * @param point         Highcharts.DataPoint, number touple or number
    * @param seriesIndex   Index position of series. This defaults to 0.
-   * @param redraw        Flag whether or not to redraw point. This defaults to false.
+   * @param redraw        Flag whether or not to redraw point. This defaults to true.
    * @param shift         Shift point to the start of series. This defaults to false.
    * @returns             Whether or not point was added successfully.
    * @memberof Chart
@@ -38,7 +38,7 @@ export class Chart {
   public addPoint(
     point: Point,
     serieIndex: number = 0,
-    redraw: boolean = false,
+    redraw: boolean = true,
     shift: boolean = false
   ): boolean {
     if (
@@ -72,16 +72,16 @@ export class Chart {
    * @param serieOptions  Series Configuration
    * @param redraw        Flag whether or not to redraw series. This defaults to true.
    * @param animation     Whether to apply animation, and optionally animation configuration. This defaults to false.
-   * @returns             Newly created series object.
+   * @returns             Whether or not serie was added successfully.
    * @memberof Chart
    */
   public addSerie(
     serie: Highcharts.SeriesOptions,
     redraw = true,
     animation: boolean | Highcharts.Animation = false
-  ): Highcharts.SeriesObject {
-    if (!serie) {
-      return null;
+  ): boolean {
+    if (!serie || !this.ref) {
+      return false
     }
 
     // init data array if not set
@@ -90,10 +90,9 @@ export class Chart {
     }
 
     this.options.series.push(serie);
+    this.ref.addSeries(serie, redraw, animation);
 
-    if (this.ref) {
-      this.ref.addSeries(serie, redraw, animation);
-    }
+    return true;
   }
 
   /**
@@ -104,24 +103,21 @@ export class Chart {
    * @memberof Chart
    */
   public removePoint(pointIndex: number, serieIndex = 0): boolean {
-    if (!this.ref) {
-      return false;
-    }
-
     const optionsPointExists =
       this.ref.options.series.length > serieIndex &&
       this.options.series[serieIndex].data.length > pointIndex;
     const refSeriesExists = this.ref.series.length > serieIndex;
 
-    if (optionsPointExists && refSeriesExists) {
-      // Remove Point from Angular Highcharts StockChart Instance Options
-      this.options.series[serieIndex].data.splice(pointIndex, 1);
-      // Remove Point in Actual Highstock Chart Instance
-      this.ref.series[serieIndex].removePoint(pointIndex, true);
-      return true;
-    } else {
+    if (!this.ref || !optionsPointExists || !refSeriesExists) {
       return false;
     }
+
+    // Remove Point from Angular Highcharts StockChart Instance Options
+    this.options.series[serieIndex].data.splice(pointIndex, 1);
+    // Remove Point in Actual Highstock Chart Instance
+    this.ref.series[serieIndex].removePoint(pointIndex, true);
+
+    return true;
   }
 
   /**
@@ -132,17 +128,17 @@ export class Chart {
    */
   public removeSerie(serieIndex: number): boolean {
     if (
-      this.ref &&
-      this.options.series.length > serieIndex &&
-      this.ref.series.length > serieIndex
+      !this.ref ||
+      !(this.options.series.length > serieIndex) ||
+      !(this.ref.series.length > serieIndex)
     ) {
-      // Case: Removing Series
-      this.options.series.splice(serieIndex, 1);
-      this.ref.series[serieIndex].remove(true);
-      return true;
-    } else {
-      // Case: Unable to Remove Series
       return false;
     }
+
+    // Case: Removing Series
+    this.options.series.splice(serieIndex, 1);
+    this.ref.series[serieIndex].remove(true);
+
+    return true;
   }
 }
