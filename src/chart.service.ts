bed6dee90cd68fe901a7092a78c0ad2e62119b29
Felix Itzenplitz <cebor@users.noreply.github.com>
2018-04-24 16:40:15 +0200

    Update chart.service.ts

--- src/chart.service.ts
+++ src/chart.service.ts
@@ -7,14 +7,13 @@
  * https://github.com/cebor/angular-highcharts/blob/master/LICENSE
  */
 import { Inject, Injectable, InjectionToken } from '@angular/core';
-
 import { Highcharts } from './highcharts';
 
 export let HIGHCHARTS_MODULES = new InjectionToken<any[]>('HighchartsModules');
 
 @Injectable()
 export class ChartService {
-  constructor(@Inject(HIGHCHARTS_MODULES) private chartModules: any[]) {}
+  constructor(@Inject(HIGHCHARTS_MODULES) private chartModules: any[]) { }
 
   initModules() {
     this.chartModules.forEach(chartModule => {
