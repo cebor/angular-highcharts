34ab4e6d06f9e43887bd50adaad02f71a53a2bab
Felix Itzenplitz <felix@stkn.org>
2020-02-11 11:24:01 +0100

    update to angular 9

--- projects/angular-highcharts/src/public-api.ts
+++ projects/angular-highcharts/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export { Chart } from './lib/chart';
+export { ChartDirective } from './lib/chart.directive';
 export { ChartModule } from './lib/chart.module';
 export { HIGHCHARTS_MODULES } from './lib/chart.service';
 export { MapChart } from './lib/mapchart';
