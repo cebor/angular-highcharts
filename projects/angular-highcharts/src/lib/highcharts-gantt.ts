914caaee12fd97501a5698caed3c7199a5d9cbc8
Anton Kalmykov <a.kalmykov@ro.ru>
2021-02-28 20:33:51 +0500

    add highcharts-gantt module (#359)
    Co-authored-by: Anton Kalmykov <a.kalmykov@reverse.systems>

---
+++ projects/angular-highcharts/src/lib/highcharts-gantt.ts
@@ -0,0 +1,43 @@
+import { ElementRef } from '@angular/core';
+import * as Highcharts from 'highcharts/highcharts-gantt';
+import { AsyncSubject, Observable } from 'rxjs';
+
+/**
+ * @license
+ * Copyright Felix Itzenplitz. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at
+ * https://github.com/cebor/angular-highcharts/blob/master/LICENSE
+ */
+export class HighchartsGantt {
+  private refSubject: AsyncSubject<Highcharts.Chart> = new AsyncSubject();
+  ref$: Observable<Highcharts.Chart> = this.refSubject.asObservable();
+  ref: Highcharts.Chart;
+
+  constructor(private options: Highcharts.Options = { series: [] }) {}
+
+  init(el: ElementRef): void {
+    if (!this.ref) {
+      Highcharts.ganttChart(el.nativeElement, this.options, chart => {
+        if (!this.ref) { // TODO: workaround for doubled callbacks on exporting charts: issue #238
+          this.refSubject.next(chart);
+          this.ref = chart;
+          this.refSubject.complete();
+        }
+      });
+    }
+  }
+
+  destroy() {
+    if (this.ref) {
+      this.options = this.ref.options;
+      this.ref.destroy();
+      this.ref = undefined;
+
+      // new init subject
+      this.refSubject = new AsyncSubject();
+      this.ref$ = this.refSubject.asObservable();
+    }
+  }
+}
