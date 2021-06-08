638a13dec7b136d3e61cad56c5cd43bbcb2f05bf
cebor <felix@stkn.org>
2021-06-08 10:57:18 +0200

    update README.md and fix DEMO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
 import more from 'highcharts/highcharts-more.src';
 import exporting from 'highcharts/modules/exporting.src';
-import stock from 'highcharts/modules/stock.src';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, ChartModule],
-  providers: [{ provide: HIGHCHARTS_MODULES, useFactory: () => [stock, more, exporting] }],
+  providers: [{ provide: HIGHCHARTS_MODULES, useFactory: () => [more, exporting] }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
