8871277d7b30925ef8f5ee19a5be3255564dea4d
yuri-apanasik <yuri.apanasik@natlex.ru>
2023-07-06 17:46:50 +0300

    Angular 16 update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lib-angular-highcharts!');
+    expect(compiled.querySelector('h1').textContent).toContain('angular-highcharts demo');
   });
 });
